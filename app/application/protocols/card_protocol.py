2e6f173c1bf94a5ae755f7a805b20cc5e9275115
Squilix01 <Squilix01@no-reply.com>
2025-10-29 14:08:36 +0200

    feat(application): add protocols for  card entity

---
+++ app/application/protocols/card_protocol.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+import datetime
+from app.domain.entity.card import Card
+
+
+class CardProtocol(ABC):
+    @abstractmethod
+    async def add(self, card: Card) -> Card:
+        ...
+
+    @abstractmethod
+    async def update(self, card: Card) -> Card:
+        ...
+
+    @abstractmethod
+    async def get_by_id(self, card_id: int, user_id: int) -> Card:
+        ...
+
+    @abstractmethod
+    async def get_due_for_review(self, user_id: int, limit: int, now: datetime) -> list[Card]:
+        ...
+
+    @abstractmethod
+    async def get_all_by_user(self, user_id: int) -> list[Card]:
+        ...
+
+
+    @abstractmethod
+    async def delete(self, card_id: int, user_id: int) -> None:
+        ...
