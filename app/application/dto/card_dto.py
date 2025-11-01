126c478645de7651da93f7df8f02052c4cd6d5f9
Squilix01 <Squilix01@no-reply.com>
2025-11-01 19:32:19 +0200

    feat(application): changed the DTO to the correct fields

--- app/application/dto/card_dto.py
+++ app/application/dto/card_dto.py
@@ -1,11 +1,16 @@
 from dataclasses import dataclass
-
+from datetime import datetime
+from typing import Optional
 
 @dataclass
 class CardDTO:
     id: int
     user_id: int
-    question: str
-    answer: str
-    created_at: str
-    updated_at: str
+    word: str
+    translation: str
+    context: Optional[str]
+    next_review_at: datetime
+    previous_interval: int
+    ease_factor: float
+    repetitions: int
+    created_at: Optional[datetime]
