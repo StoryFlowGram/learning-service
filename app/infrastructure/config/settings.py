c0fb764d7a0129248b5903f4e6d17a3c93c674a6
Squilix <Squilix@example.com>
2026-01-11 14:01:29 +0300

    feat(infra): change config

--- app/infrastructure/config/settings.py
+++ app/infrastructure/config/settings.py
@@ -3,18 +3,18 @@ from sqlalchemy import URL
 
 
 class Database(BaseSettings):
-    DB_USER: str
-    DB_PASSWORD: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
     DB_HOST: str
-    DB_NAME: str
 
     def get_database_url(self, DB_API) -> URL:
         return URL.create(
             drivername=f"postgresql+{DB_API}",
-            username=self.DB_USER,
-            password=self.DB_PASSWORD,
-            host=self.DB_HOST,
-            database=self.DB_NAME
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_DB,
+            database=self.DB_HOST
         )
     
     model_config = {
@@ -22,19 +22,7 @@ class Database(BaseSettings):
         "env_file_encoding": "utf-8"
     }
 
-class JWTConfig(BaseSettings):
-    JWT_ALGORITHM: str
-    JWT_SECRET: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
-    REFRESH_TOKEN_EXPIRE_DAYS: int =  14
-
-    model_config = {
-        "extra": "ignore",
-        "env_file_encoding": "utf-8",
-    }
-
 
 class Settings:
     def __init__(self, env_file: str | None = None):
         self.database = Database(_env_file=env_file)
-        self.jwt = JWTConfig(_env_file=env_file)
