d4da1d62ad3337555e6937ae7470ee8525e6e057
Squilix <Squilix@example.com>
2026-01-11 14:04:29 +0300

    refactor(usecase): refactor usecase for update users card

--- app/application/usecase/update_card_usecase.py
+++ app/application/usecase/update_card_usecase.py
@@ -4,31 +4,35 @@ from app.application.dto.card_dto import CardDTO
 from app.domain.exception.card_exceptions import CardNotFoundException
 
 
-# Оставить про запас Любой код с Update
+from app.application.protocols.uow import IUnitOfWork
+from app.domain.value_object.review_quality_vo import ReviewQuality
+from app.domain.exception.card_exceptions import CardNotFoundException
+from app.application.dto.card_dto import CardDTO
 
-class UpdateCardUseCase:
+class ReviewCardUseCase:
     def __init__(self, uow: IUnitOfWork):
         self.uow = uow
 
-    async def __call__(self, card: Card):
+    async def __call__(self, user_id: int, card_id: int, quality_value: int) -> CardDTO:
         async with self.uow:
-            check_existing = await self.uow.cards.get_by_id(card.id, card.user_id)
-            if not check_existing:
-                raise CardNotFoundException(card_id=card.id, user_id=card.user_id)
-
-            update_card = await self.uow.cards.update(card)
-            if not update_card:
-                raise Exception(f"Не удалось обновить карту с id {card.id}")
+            card = await self.uow.cards.get_by_id(card_id=card_id, user_id=user_id)
             
-        return CardDTO(
-            id=update_card.id,
-            user_id=update_card.user_id,
-            word=update_card.word,
-            translation=update_card.translation,
-            context=update_card.context,
-            next_review_at=update_card.next_review_at,
-            previous_interval=update_card.previous_interval.days,
-            ease_factor=update_card.ease_factor,
-            repetitions=update_card.repetitions,
-            created_at=update_card.created_at
-        )
+            if not card:
+                raise CardNotFoundException(card_id=card_id, user_id=user_id)
+
+            quality = ReviewQuality(value=quality_value)
+            card.update_srs_state(quality)
+            updated_card = await self.uow.cards.update(card)
+            await self.uow.commit() 
+            return CardDTO(
+                id=updated_card.id,
+                user_id=updated_card.user_id,
+                word=updated_card.word,
+                translation=updated_card.translation,
+                context=updated_card.context,
+                next_review_at=updated_card.next_review_at,
+                previous_interval=updated_card.previous_interval.days,
+                ease_factor=updated_card.ease_factor,
+                repetitions=updated_card.repetitions,
+                created_at=updated_card.created_at
+            )
